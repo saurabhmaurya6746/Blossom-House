0e0c58823332c04dbe63342fe13b49d1ab531345
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2025-07-13 17:35:38 +0530

    Update settings.py

--- BlossomHouseSchool/settings.py
+++ BlossomHouseSchool/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -113,15 +114,14 @@ USE_TZ = True
 USE_I18N = True
 
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'  # add trailing slash
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # this is for production
 
-# This is used only in development (you can comment or delete in production)
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Required when using WhiteNoise (optional but recommended)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
